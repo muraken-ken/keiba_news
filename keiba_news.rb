7673a19467ccfc5c37d2284938986af8725c468d
muraken-ken <qzs04130@gmail.com>
2021-10-28 13:29:21 +0900

    Add support for the saving file, log and the check duplex
    - ファイルとログのsave機能の追加
    - 重複チェック機能の追加
    - ファイルパスを実使用に合わせて変更

--- keiba_news.rb
+++ keiba_news.rb
@@ -1,40 +1,61 @@
 # 【名  称】競馬実況ホームページ記事抽出スクリプト
 # 【仕  様】ラジオNIKKEI競馬実況ホームページから記事データを読み込み
 #           馬名毎に記事ファイルを編集出力する
-# 【来  歴】Ver 0.01 試作（蛸坊主さんのスクリプトを参考に作成）
+# 【来  歴】Ver 0.1 試作（蛸坊主さんのスクリプトを参考に作成）
 
 require 'nokogiri'
 require 'open-uri'
 require 'pp'
 
-URL = 'http://keiba.radionikkei.jp/keiba/news/headline/'
+URL = 'http://keiba.radionikkei.jp/keiba/news/headline/'.freeze
 
-def get_html(url)
-  url = URI.parse(url)
-  Nokogiri::HTML(URI.open(url))
+def open_html(url)
+  Nokogiri::HTML(URI.open(URI.parse(url)))
 end
 
-news_list = get_html(URL)
-sleep 5
-
-url_list = []
-news_list.css('.detail_kjnet_kiji ul li a').each do |elem|
-  url_list << elem[:href]
+def url_list(url)
+  news_list = open_html(url)
+  puts 'サイトにアクセスしています...'
+  sleep 2
+  url_list = []
+  news_list.css('.detail_kjnet_kiji ul li a').each do |elem|
+    url_list << elem[:href]
+  end
+  url_list.reverse
 end
 
-url_list.reverse!
+def done?(file_name, str)
+  File.open("/mnt/c/TFJV/EX_DATA/#{file_name}", 'a+:sjis:utf-8').read.include?(str)
+end
 
-article = get_html(url_list[5])
-sleep 5
-title = article.css('.detail_kjnet_kiji dl dt').text
-update_at = article.css('.detail_kjnet_kiji dl dd').text
-body = article.css('.detail_topics').inner_html.gsub!(/<br>/, "\n")
+url_list = url_list(URL)
 
-pp title
-pp update_at
-pp body
+url_list.each do |url|
+  article = open_html(url)
+  sleep 2
+  p title = article.css('.detail_kjnet_kiji dl dt').text
+  update_at = article.css('.detail_kjnet_kiji dl dd').text
+  next if done?('log.txt', update_at)
 
+  body = article.css('.detail_topics').inner_html.gsub!(/<br>/, "\n")
+  horse_names = body.scan(/\p{Katakana}[\p{Katakana}ー]{1,8}/).uniq
+  next unless horse_names
 
-# news_url_lists.each do |elem|
-#   puts url = elem[:href]
-# end
+  horse_names.each do |name|
+    p file_name = "#{name}.txt"
+    next if done?(file_name, update_at)
+
+    File.open("/mnt/c/TFJV/EX_DATA/#{file_name}", 'a+:sjis:utf-8') do |f|
+      f.puts title
+      f.puts
+      f.puts "■ #{update_at}"
+      f.puts body
+      f.puts '【出典】ラジオNIKKEI競馬実況web http://keiba.radionikkei.jp/'
+      f.puts "【日時】#{Time.now.strftime('%Y/%m/%d %H:%M:%S')}"
+      f.puts '-' * 80
+    end
+  end
+  File.open('/mnt/c/TFJV/EX_DATA/log.txt', 'a+:sjis:utf-8') do |f|
+    f.puts update_at
+  end
+end
